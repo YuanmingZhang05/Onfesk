9f2038fa02a9ea0f5d71ea5e9912bf6c22179403
YuanmingZhang05 <106535611+YuanmingZhang05@users.noreply.github.com>
2025-03-13 15:06:48 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <string>
 #include "train.h"
+/*
+You may replace the "train.h" with "benchmark.h" for inputting parameters without modifying the codes.  
+*/
 const int batch_size = 10000;
 int parse_svm(std::fstream &fin, std::vector<std::vector<double>> &data, std::vector<std::vector<int>> &indices, std::vector<int> &labels)
 {
@@ -50,12 +53,12 @@ int main(int argc, char **argv)
     std::vector<std::vector<double>> data(batch_size);
     std::vector<std::vector<int>> indices(batch_size);
     std::vector<int> labels(batch_size);
-    int k = 256, m = 32;
-    double p = 1.;
+    int k = 256, m = 32; //The sketch length k and the approximate one-hot representation length m.
+    double p = 1.; //The tuning parameter for the kernel space.
     Onfesk::UpdateStrategy method = Onfesk::UpdateStrategy::PA;
     OnfeskClassifier::SketchType sketch_type = OnfeskClassifier::SketchType::FastGM;
     OnfeskClassifier classifier(k, m, p, method, sketch_type);
-    std::fstream fin("news20", std::fstream::in);  
+    std::fstream fin("input_file_dir", std::fstream::in);  //Replace “input_file_dir” with the correct file dir.
     while(parse_svm(fin, data, indices, labels) == batch_size)
         classifier.evaluate(data, indices, labels);
     classifier.evaluate(data, indices, labels);
