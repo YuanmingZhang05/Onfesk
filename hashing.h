b9be2991a48fe004878400233ca12be51660b403
YuanmingZhang05 <106535611+YuanmingZhang05@users.noreply.github.com>
2025-05-21 18:20:30 +0800

    Update hashing.h

--- hashing.h
+++ hashing.h
@@ -1,6 +1,6 @@
 #include<mkl.h>
 #include"mmh3.h"
-double uniform_hash(uint32_t key, uint32_t seed);
-double exponential_hash(uint32_t key, uint32_t seed);
-int uniformint_hash(uint32_t key, uint32_t seed, int low, int high);
-double gamma21_hash(uint32_t key, uint32_t seed);
+double uniform_hash(uint32_t key, uint32_t seed); // generate a uniform random variable
+double exponential_hash(uint32_t key, uint32_t seed); // generate an exponential random variable
+int uniformint_hash(uint32_t key, uint32_t seed, int low, int high); // uniformly select a int variable from (int low, int high)
+double gamma21_hash(uint32_t key, uint32_t seed); // generate a Gamma random variable
