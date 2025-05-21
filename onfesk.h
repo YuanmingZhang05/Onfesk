5cd4810d95468fc04f7e1ed797d1580a3a4d8d84
YuanmingZhang05 <106535611+YuanmingZhang05@users.noreply.github.com>
2025-05-21 18:23:16 +0800

    Update onfesk.h

--- onfesk.h
+++ onfesk.h
@@ -5,8 +5,7 @@ class Onfesk
 {
 public:
 	std::vector<std::vector<double>> weights_;
-	enum class UpdateStrategy { Perceptron, PA, SVM, LR };
-	int k_, m_;
-	Onfesk(int k, int m);
+	enum class UpdateStrategy { Perceptron, PA, SVM, LR }; \\ online update methods
+	Onfesk(int k, int m); \\ k is the sketch length and m is the approximate one-hot reprensentaion length
 	bool update(std::vector<int> s, int label, UpdateStrategy method);
 };
