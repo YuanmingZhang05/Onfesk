234ede825a41ae4843fd9a4fbc922758eab40796
YuanmingZhang05 <106535611+YuanmingZhang05@users.noreply.github.com>
2025-05-21 18:24:54 +0800

    Update sketching.h

--- sketching.h
+++ sketching.h
@@ -7,6 +7,5 @@
 #include<cfloat>
 //#include<boost/math/special_functions/gamma.hpp>
 #include"hashing.h"
-std::vector<int> fastgm(int k, std::vector<double> v, std::vector<int> indices);
-std::vector<int> ICWS(int k, std::vector<double> v, std::vector<int> indices);
-//std::vector<int> fastgm_alpha(int k, double alpha, std::vector<double> v, std::vector<int> indices);
+std::vector<int> fastgm(int k, std::vector<double> v, std::vector<int> indices); \\ feature sketch generation method of GSM kernel
+std::vector<int> ICWS(int k, std::vector<double> v, std::vector<int> indices); \\ feature sketch generation method of GMM kernel
